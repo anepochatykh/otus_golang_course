5b6cafd709067d83cd4a8f56217da2d61248c2cb
Aleksandr Nepochatykh <aanepochatykh@avito.ru>
2022-07-14 23:22:54 +0300

    Refactoring

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -7,8 +7,8 @@ import (
 )
 
 type wordCountStruct struct {
-	w string
-	c int32
+	w string // word
+	c int32  // word count (frequency)
 }
 
 const TopNumber = 10
@@ -20,7 +20,8 @@ func min(a, b int) int {
 	return b
 }
 
-func Top10(inp string) []string {
+// Get token frequency as a map.
+func getTokenFrequency(inp string) map[string]int32 {
 	tokenFrequency := make(map[string]int32)
 	r := regexp.MustCompile("[a-яА-Я-]+")
 	tokens := strings.Fields(inp)
@@ -29,22 +30,44 @@ func Top10(inp string) []string {
 			tokenFrequency[t]++
 		}
 	}
+	return tokenFrequency
+}
+
+// Get sorted wordCounts as a slice of wordCountStruct.
+func getWordCounts(tokenFrequency map[string]int32) []wordCountStruct {
+	// form wordCounts
 	wordCounts := []wordCountStruct{}
 	for key, val := range tokenFrequency {
 		wordCounts = append(wordCounts, wordCountStruct{w: key, c: val})
 	}
 
-	maxRetLen := min(TopNumber, len(wordCounts))
-
+	// sort wordCounts
 	sort.Slice(wordCounts, func(i, j int) bool {
 		if wordCounts[i].c == wordCounts[j].c {
 			return wordCounts[i].w < wordCounts[j].w
 		}
 		return wordCounts[i].c > wordCounts[j].c
 	})
-	result := make([]string, maxRetLen)
-	for i, curWordCount := range wordCounts[:maxRetLen] {
+
+	return wordCounts
+}
+
+// Get topN words with max frequency.
+func getTopWords(wordCounts []wordCountStruct, topN int) []string {
+	result := make([]string, topN)
+	for i, curWordCount := range wordCounts[:topN] {
 		result[i] = curWordCount.w
 	}
 	return result
+}
+
+func Top10(inp string) []string {
+	// get token frequency as map
+	tokenFrequency := getTokenFrequency(inp)
+	// get sorted wordCounts as a slice of wordCountStruct
+	wordCounts := getWordCounts(tokenFrequency)
+	// get max ret len
+	maxRetLen := min(TopNumber, len(wordCounts))
+	// get topN words with max frequency
+	return getTopWords(wordCounts, maxRetLen)
 }
