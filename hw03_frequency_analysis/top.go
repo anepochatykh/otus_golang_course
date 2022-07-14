cf061ee363cba86797f25271c8e95f71f2d3425c
Aleksandr Nepochatykh <aanepochatykh@avito.ru>
2022-07-14 23:03:07 +0300

    Made standard tests pass

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,50 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordCountStruct struct {
+	w string
+	c int32
+}
+
+const TopNumber = 10
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Top10(inp string) []string {
+	tokenFrequency := make(map[string]int32)
+	r := regexp.MustCompile("[a-яА-Я-]+")
+	tokens := strings.Fields(inp)
+	for _, t := range tokens {
+		if r.MatchString(t) {
+			tokenFrequency[t]++
+		}
+	}
+	wordCounts := []wordCountStruct{}
+	for key, val := range tokenFrequency {
+		wordCounts = append(wordCounts, wordCountStruct{w: key, c: val})
+	}
+
+	maxRetLen := min(TopNumber, len(wordCounts))
+
+	sort.Slice(wordCounts, func(i, j int) bool {
+		if wordCounts[i].c == wordCounts[j].c {
+			return wordCounts[i].w < wordCounts[j].w
+		}
+		return wordCounts[i].c > wordCounts[j].c
+	})
+	result := make([]string, maxRetLen)
+	for i, curWordCount := range wordCounts[:maxRetLen] {
+		result[i] = curWordCount.w
+	}
+	return result
 }
