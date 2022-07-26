41973cddd25ef783973ca811602f15b8f62dc780
Aleksandr Nepochatykh <aanepochatykh@avito.ru>
2022-07-26 16:02:21 +0300

    Init commit with passed tests

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,63 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+func safeIncrement(m *sync.Mutex, errorsTotal *int) {
+	m.Lock()
+	defer m.Unlock()
+	*errorsTotal++
+}
+
+func ifErrorsExceed(m *sync.Mutex, errorsTotal *int, maxError int) bool {
+	m.Lock()
+	defer m.Unlock()
+	return *errorsTotal >= maxError
+}
+
+func worker(w *sync.WaitGroup, m *sync.Mutex, in chan Task, errorsTotal *int, maxError int) {
+	defer w.Done()
+	for task := range in {
+		if err := task(); err != nil {
+			safeIncrement(m, errorsTotal)
+		}
+		if ifErrorsExceed(m, errorsTotal, maxError) {
+			return
+		}
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	// todo - m < 0
+
+	w := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
+	w.Add(n) // no more than n tasks
+	inChannels := make(chan Task, len(tasks))
+
+	// push tasks to channels
+	for i := 0; i < len(tasks); i++ {
+		inChannels <- tasks[i]
+	}
+	close(inChannels)
+
+	// start workers
+	var errorsTotal int
+	for i := 0; i < n; i++ {
+		go worker(w, mut, inChannels, &errorsTotal, m)
+	}
+	w.Wait()
+
+	// return
+	var result error
+	if errorsTotal >= m {
+		result = ErrErrorsLimitExceeded
+	}
+	return result
 }
